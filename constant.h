e2c152df72fbb5b0eb4880a8f55187d71aca66ab
wongyt2008 <wongyt2014@yahoo.com.hk>
2014-11-13 11:33:10 +0800

    Create constant.h

---
+++ constant.h
@@ -0,0 +1,13 @@
+@interface constant : NSObject
+
+FOUNDATION_EXPORT NSString *const S3KeyName;
+
+FOUNDATION_EXPORT NSString *const S3BucketName;
+
+
+FOUNDATION_EXPORT  NSString *const AWSAccountID;
+FOUNDATION_EXPORT  NSString *const CognitoPoolID;
+FOUNDATION_EXPORT  NSString *const CognitoRoleAuth;
+FOUNDATION_EXPORT  NSString *const CognitoRoleUnauth;
+
+@end
