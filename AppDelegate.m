687cd22d1b1ffad4658159dd087e37834a008b91
wongyt2008 <wongyt2014@yahoo.com.hk>
2014-11-13 11:27:51 +0800

    Update AppDelegate.m

--- AppDelegate.m
+++ AppDelegate.m
@@ -17,10 +17,10 @@
     
     AWSCognitoCredentialsProvider *credentialsProvider = [AWSCognitoCredentialsProvider
                                                           credentialsWithRegionType:AWSRegionUSEast1
-                                                          accountId:@"613437952972"
-                                                          identityPoolId:@"us-east-1:d2487b94-d010-4c5d-b245-8eebd46bc300"
-                                                          unauthRoleArn:@"arn:aws:iam::613437952972:role/Cognito_CognitoTestingUnauth_DefaultRole"
-                                                          authRoleArn:@"arn:aws:iam::613437952972:role/Cognito_CognitoTestingAuth_DefaultRole"];
+                                                          accountId:@"XXXXXXX"
+                                                          identityPoolId:@"us-east-1:XXXXXXXXXXX"
+                                                          unauthRoleArn:@"arn:aws:iam::XXXXXXXX:role/Cognito_CognitoTestingUnauth_DefaultRole"
+                                                          authRoleArn:@"arn:aws:iam::XXXXXXXXXXX:role/Cognito_CognitoTestingAuth_DefaultRole"];
     
     AWSServiceConfiguration *configuration = [AWSServiceConfiguration configurationWithRegion:AWSRegionUSEast1
                                                                           credentialsProvider:credentialsProvider];
